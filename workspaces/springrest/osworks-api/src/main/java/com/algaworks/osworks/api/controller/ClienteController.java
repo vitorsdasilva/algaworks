1a18b750dd94238a0cd7efe473fc8f6a5235eae0
Vitor Santos <vitors.dasilva@gmail.com>
2020-04-30 01:40:11 -0300

    Validação e tratamento de Exception

--- workspaces/springrest/osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ workspaces/springrest/osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -3,6 +3,8 @@ package com.algaworks.osworks.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -44,13 +46,13 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
 	
 	@PutMapping("/{clienteId}")
-	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, 
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId, 
 			@RequestBody Cliente cliente){
 		
 		if (!clienteRepository.existsById(clienteId)) {

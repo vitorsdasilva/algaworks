6e01a353c622d794269fa50705b088bf6b8a45b8
Vitor Santos <vitors.dasilva@gmail.com>
2020-05-02 01:27:35 -0300

    Criando mensagens de registro e Finalizando as Ordens de Serviço

--- workspaces/springrest/osworks-api/src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
+++ workspaces/springrest/osworks-api/src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
@@ -2,6 +2,9 @@ package com.algaworks.osworks.domain.model;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -9,6 +12,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.algaworks.osworks.api.model.Comentario;
+import com.algaworks.osworks.domain.exception.NegocioException;
 
 @Entity
 public class OrdemServico {
@@ -26,6 +33,10 @@ public class OrdemServico {
 	private StatusOrdemServico status;
 	private OffsetDateTime dataAbertura; 
 	private OffsetDateTime dataFinalizacao;
+	
+	@OneToMany(mappedBy = "ordemServico")
+	private List<Comentario> comentarios = new ArrayList<>();
+	
 	
 	public Long getId() {
 		return id;
@@ -68,6 +79,12 @@ public class OrdemServico {
 	}
 	public void setDataFinalizacao(OffsetDateTime dataFinalizacao) {
 		this.dataFinalizacao = dataFinalizacao;
+	}	
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
 	}
 	@Override
 	public int hashCode() {
@@ -91,6 +108,24 @@ public class OrdemServico {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+	
+	public boolean podeSerFinalizada() {
+		return StatusOrdemServico.ABERTA.equals(getStatus());
+	}
+	
+	public boolean naoPodeSerFinalizada() {
+		return !podeSerFinalizada();
+	}
+	
+	public void finalizar() {	
+		if(!podeSerFinalizada()){
+			throw new NegocioException ("Ordem de serviço não pode ser finalizada");
+			
+		}
+		setStatus(StatusOrdemServico.FINALIZADA);
+		setDataFinalizacao(OffsetDateTime.now());
+		
 	}
 	
 	
--- workspaces/springrest/osworks-api/src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
+++ workspaces/springrest/osworks-api/src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
@@ -5,11 +5,14 @@ import java.time.OffsetDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.osworks.api.model.Comentario;
+import com.algaworks.osworks.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.osworks.domain.exception.NegocioException;
 import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.model.OrdemServico;
 import com.algaworks.osworks.domain.model.StatusOrdemServico;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
+import com.algaworks.osworks.domain.repository.ComentarioRepository;
 import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 
 @Service
@@ -20,6 +23,9 @@ public class GestaoOrdemServicoService {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ComentarioRepository comentarioRepository;
 	
 	public OrdemServico criar(OrdemServico ordemServico) {
 		Cliente cliente = clienteRepository.findById(ordemServico.getId())
@@ -32,5 +38,33 @@ public class GestaoOrdemServicoService {
 		
 		return ordemServicoRepository.save(ordemServico); 
 	}
-
+	
+	public void finalizar (Long ordemServicoId) {
+		
+		OrdemServico ordemServico = buscar(ordemServicoId);
+		
+		ordemServico.finalizar();
+		
+		
+		ordemServicoRepository.save(ordemServico);
+		
+	}
+	
+	public Comentario adicionarComentario(Long ordemServicoId, String descricao) {
+		OrdemServico ordemServico = buscar(ordemServicoId);
+		
+		Comentario comentario = new Comentario();
+		comentario.setDataEnvio(OffsetDateTime.now());
+		comentario.setDescricao(descricao);
+		comentario.setOrdemServico(ordemServico);
+		
+		return comentarioRepository.save(comentario);
+		
+	}
+	
+	private OrdemServico buscar(Long ordemServicoId) {
+		return ordemServicoRepository.findById(ordemServicoId)
+				.orElseThrow(() -> new EntidadeNaoEncontradaException("Ordem de serviço não encontrada"));
+	}
+	
 }
